d8ec813e75173dcaae64610f0b7b8db479784bad
Imran Remtulla <contact@imranr.dev>
2025-08-12 15:46:01 -0400

    Add secureblue support (untested)

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Run an [FRP](https://github.com/fatedier/frp) client during system boot using [D
 
 Meant for use with [dracut-crypt-ssh](https://github.com/dracut-crypt-ssh/dracut-crypt-ssh) to remotely decrypt a Fedora server that is behind [NAT](https://en.wikipedia.org/wiki/Network_address_translation).
 
+Also supports [secureblue](https://secureblue.dev/).
+
 ## Usage
 
 1. Prerequisites:
    - Ensure a network connection is available in the pre-boot environment.
      - For a wired connection on systems that use GRUB, this is as simple as adding `rd.neednet=1 ip=dhcp` to the `GRUB_CMDLINE_LINUX` variable in `/etc/default/grub`, then running `grub2-mkconfig --output /etc/grub2.cfg`.
-   - Ensure whatever services you'll use FRP for (for example, an SSH service), are also setup to run in the initramfs environment.
+   - Ensure whatever services you'll use FRP for (for example, an SSH service via `dracut-crypt-ssh`), are also already setup to run in the initramfs environment.
 2. Run `setup.sh`. This will:
    1. Download the latest version of FRPC.
    2. Grab a user-defined FRPC config file.
--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ set -e
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+SUDO_COMMAND="sudo"
+if which rpm-ostree; then SUDO_COMMAND="run0"; fi # Support secureblue
+
 # Download the latest version of FRPC and add it to the module
 FRPVER="$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep -oE 'tag/.*' | grep -oE '[0-9]+(\.[0-9]+)*')"
 if [ -z "$FRPVER" ]; then
@@ -28,15 +31,19 @@ cp "$FRPC_INI" "$HERE"/modules/99frpc/frpc.ini
 
 # Add the module to dracut
 MODULE_DIR=/usr/lib/dracut/modules.d/99frpc
-if [ -d "$MODULE_DIR" ]; then sudo rm -r "$MODULE_DIR"; fi
-sudo mkdir -p "$MODULE_DIR"
-sudo cp "$HERE"/modules/99frpc/* "$MODULE_DIR"
-sudo chmod +x "$MODULE_DIR"/*.sh
+if [ -d "$MODULE_DIR" ]; then $SUDO_COMMAND rm -r "$MODULE_DIR"; fi
+$SUDO_COMMAND mkdir -p "$MODULE_DIR"
+$SUDO_COMMAND cp "$HERE"/modules/99frpc/* "$MODULE_DIR"
+$SUDO_COMMAND chmod +x "$MODULE_DIR"/*.sh
 
 # Build the module
 echo "About to build the module. Press Enter to continue."
 read anything
-sudo dracut -f -v
+if [ "$SUDO_COMMAND" == "run0" ]; then
+    $SUDO_COMMAND dracut -f -v
+else
+    rpm-ostree initramfs --enable --arg "--force"
+fi
 
 # Conclusion
 echo "Use 'lsinitrd | less' and 'lsinitrd -f <file>' to explore the built image."
