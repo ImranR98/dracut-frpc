bd4ce0f4ee069eb32cca0d5d88026e6deb750a44
Imran <30463115+ImranR98@users.noreply.github.com>
2025-09-14 14:42:08 -0400

    Update installNew.sh

--- rpm_files/installNew.sh
+++ rpm_files/installNew.sh
@@ -7,7 +7,7 @@ if [ -z "$TIMESTAMP" ]; then exit 1; fi
 
 rpm-ostree initramfs --enable --arg "--force"
 
-if rpm-ostree status | grep LocalForcedPackages | head -1 | grep -q my_dracut_frpc; then
+if rpm-ostree status | grep -q my_dracut_frpc; then
     rpm-ostree install --apply-live --assumeyes ~/rpmbuild/RPMS/x86_64/my_dracut_frpc-0.0.$TIMESTAMP-1.fc42.x86_64.rpm --uninstall my_dracut_frpc --allow-inactive --force-replacefiles || :
 else
     rpm-ostree install --apply-live --assumeyes ~/rpmbuild/RPMS/x86_64/my_dracut_frpc-0.0.$TIMESTAMP-1.fc42.x86_64.rpm
